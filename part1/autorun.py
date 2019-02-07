8dc95a714d4d003f3edbaca27a2120b5b425aac7
Beth <elaolive@ucsc.edu>
2019-02-06 23:32:17 -0800

    added parameters
    But you can't save the model or load them yet

---
+++ part1/autorun.py
@@ -0,0 +1,30 @@
+# AutoTest some stuff:
+from tfRNN import trainAndGenerate
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+modelsFolder = "models/"
+pickle = ".pickle"
+
+inputFile="lordOfTheRingsReformatted.txt"
+pickle_model = "LOTR_model"
+epochs = 1
+
+string_starts = [ "Frodo ", "Sam", "When ", "It was", "As for ", "Mr. Bilbo Baggins ", "All that day "]
+
+results = {}
+
+for epochs in [1, 3, 5, 10, 15, 25]:
+	test_name = "%s_e%s"%(pickle_model, epochs)
+	pickle_model_path = modelsFolder+test_name+pickle
+	trainAndGenerate(inputFile, None, None, pickle_model_path,
+	                epochs=epochs, checkpoint_dir='./training_checkpoints/%s'%test_name )
+
+	result[epochs] = {}
+	for string in string_starts:
+		result[epochs][string] = {}
+		for temperature in [0.01, 0.1, 0.5, 1.0, 1.5, 2, 5, 10]
+			result[epochs][string][temperature] = trainAndGenerate(None, pickle_model_path, string, None, temperature=temperature)
+
+pp.pprint(results)
--- part1/tfRNN.py
+++ part1/tfRNN.py
@@ -8,14 +8,14 @@ import numpy as np
 import os
 import time
 
-#import argparse
-
+import argparse
+import pickle
 
 #of the variables, I think only seq_length might be interesting to fiddle with
 
 def processInput(path_to_file, seq_length=100, BATCH_SIZE=64, BUFFER_SIZE=1000):
     # Read in the text
-    text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+    text = open(path_to_file, 'rb').read().decode(encoding='utf-8')[:10000]
 
     # Get the unique characters in the file
     vocab = sorted(set(text))
@@ -58,6 +58,26 @@ def processInput(path_to_file, seq_length=100, BATCH_SIZE=64, BUFFER_SIZE=1000):
 
     return data
 
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    if tf.test.is_gpu_available():
+        rnn = tf.keras.layers.CuDNNGRU
+    else:
+        import functools
+        rnn = functools.partial(
+                tf.keras.layers.GRU, recurrent_activation='sigmoid')
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                  batch_input_shape=[batch_size, None]),
+        rnn(rnn_units,
+            return_sequences=True,
+            recurrent_initializer='glorot_uniform',
+            stateful=True),
+
+        tf.keras.layers.Dense(vocab_size)
+    ])
+    return model
+
 def buildModel(data, embedding_dim=256, rnn_units=1024):
     vocab = data["vocab"]
     BATCH_SIZE = data["BATCH_SIZE"]
@@ -66,47 +86,31 @@ def buildModel(data, embedding_dim=256, rnn_units=1024):
     # Length of the vocabulary in chars
     vocab_size = len(vocab)
 
-    if tf.test.is_gpu_available():
-        rnn = tf.keras.layers.CuDNNGRU
-    else:
-        import functools
-        rnn = functools.partial(
-                tf.keras.layers.GRU, recurrent_activation='sigmoid')
-
-        def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-            model = tf.keras.Sequential([
-                tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                                          batch_input_shape=[batch_size, None]),
-                rnn(rnn_units,
-                    return_sequences=True,
-                    recurrent_initializer='glorot_uniform',
-                    stateful=True),
-
-                tf.keras.layers.Dense(vocab_size)
-            ])
-            return model
-
-        model = build_model(
-                  vocab_size = len(vocab), 
-                  embedding_dim=embedding_dim, 
-                  rnn_units=rnn_units, 
-                  batch_size=BATCH_SIZE)
+    model = build_model(
+              vocab_size = len(vocab), 
+              embedding_dim=embedding_dim, 
+              rnn_units=rnn_units, 
+              batch_size=BATCH_SIZE)
 
     model.summary()
 
     return model
 
+# define our loss function:
+def loss(labels, logits):
+    return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+optimizer = tf.train.AdamOptimizer
+
 def trainModel(model, data, EPOCHS=3, checkpoint_dir='./training_checkpoints'):
     dataset = data["dataset"]
     steps_per_epoch = data["steps_per_epoch"]
+    vocab_size = len(data["vocab"])
 
-    # define our loss function:
-    def loss(labels, logits):
-        return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
-
+    
     #configures the training procedure 
     model.compile(
-        optimizer = tf.train.AdamOptimizer(), 
+        optimizer = optimizer, 
         loss = loss)
     
     # Name of the checkpoint files
@@ -128,6 +132,8 @@ def trainModel(model, data, EPOCHS=3, checkpoint_dir='./training_checkpoints'):
 
     
     model.summary()
+
+    
     return model
 
 
@@ -166,12 +172,84 @@ def generateText(model, data, start_string, num_generate=1000, temperature=1.0):
 
   return (start_string + ''.join(text_generated))
 
+def trainAndGenerate(inputFile, model, start_string, pickle_model,
+                seq_length=100, BATCH_SIZE=64, BUFFER_SIZE=1000,
+                embedding_dim=256, rnn_units=1024,
+                epochs=3, checkpoint_dir='./training_checkpoints',
+                num_generate=1000, temperature=1.0 ):
+
+
+    if model:
+        checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
+        checkpoint.restore(tf.train.latest_checkpoint(model))
+    else:
+        if not inputFile:
+            inputFile = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+        data = processInput(inputFile, seq_length=seq_length, BATCH_SIZE=BATCH_SIZE, BUFFER_SIZE=BUFFER_SIZE)
+
+        model = buildModel(data, embedding_dim=embedding_dim, rnn_units=rnn_units)
+        model = trainModel(model, data, EPOCHS=epochs, checkpoint_dir=checkpoint_dir)
+
+        if pickle_model:
+            checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
+            checkpoint.save(pickle_model)
+
+    if start_string:
+        return generateText(model, data, start_string, num_generate=num_generate, temperature=temperature)
+
+
 if __name__== "__main__":
-    inputFile = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
-    start_string = u"ROMEO: "
+    parser = argparse.ArgumentParser(description='Builds and Trains an RNN built with TensorFlow, as well as generates new text based on the model.')
 
-    data = processInput(inputFile)
-    model = buildModel(data["vocab"])
-    model = trainModel(model, data)
+    parser.add_argument("--input_text_path", "-t", help="Location of the raw text.")
+    parser.add_argument("--start_string", "-s", help="String to start generating on.")
 
-    print(generateText(model, data, start_string))
+    parser.add_argument("--pretrained_model_path", "-m", help="Location of pretrained_model.")
+    parser.add_argument("--save_trained_model", help="Location to save trained model.")
+
+    # preprocessing arguments:
+    parser.add_argument("--sequence_length", default=100, type=int, help="Length of the context used to train the model.")
+    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--buffer_size", default=1000, type=int)
+
+
+    # model arguements:
+    parser.add_argument("--epochs", "-e", default=3, type=int, help="Number of iterations to train over")
+    parser.add_argument("--checkpoint_dir", default='./training_checkpoints')
+    parser.add_argument("--embedding_dim", default=256, type=int)
+    parser.add_argument("--rnn_units", default=1024, type=int)
+
+    # generation arguements:
+    parser.add_argument("--output_length", "-o", default=1000, type=int)
+    parser.add_argument("--temperature", default=1.0, type=float, help="Influences how 'predictable' the text output will be. Low temperatures results in more predictable text. Higher temperatures results in more surprising text. Experiment to find the best setting.")
+
+
+
+    args = parser.parse_args()
+    inputFile = args.input_text_path
+    start_string = args.start_string
+    
+    model = args.pretrained_model_path
+    pickle_model = args.save_trained_model
+
+    seq_length = args.sequence_length
+    batch_size = args.batch_size
+    buffer_size = args.buffer_size
+
+    embedding_dim = args.embedding_dim
+    rnn_units = args.rnn_units
+    epochs = args.epochs
+    checkpoint_dir = args.checkpoint_dir
+
+    output_length = args.output_length
+    temp = args.temperature
+
+
+    print(trainAndGenerate(inputFile, model, start_string, pickle_model=pickle_model,
+                                seq_length=seq_length, BATCH_SIZE=batch_size, BUFFER_SIZE=buffer_size,
+                                embedding_dim=embedding_dim, rnn_units=rnn_units,
+                                epochs=epochs, checkpoint_dir=checkpoint_dir,
+                                num_generate=output_length, temperature=temp ))
+
+    
