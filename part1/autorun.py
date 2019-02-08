804abfb66d060d38b87290f59d23de9d30c1df3d
Beth <elaolive@ucsc.edu>
2019-02-07 17:03:15 -0800

    small cleaning changes

--- part1/autorun.py
+++ part1/autorun.py
@@ -18,14 +18,14 @@ results = {}
 for epochs in [1, 3, 5, 10, 15, 25]:
 	test_name = "%s_e%s"%(pickle_model, epochs)
 	pickle_model_path = modelsFolder+test_name+pickle
-	trainAndGenerate(inputFile, None, None, pickle_model_path,
-	                epochs=epochs, checkpoint_dir='./training_checkpoints/%s'%test_name )
+	trainAndGenerate(inputFile, None, None,
+	                epochs=epochs, checkpoint_dir=pickle_model_path )
 
 	result[epochs] = {}
 	for string in string_starts:
 		result[epochs][string] = {}
 		for temperature in [0.01, 0.1, 0.5, 1.0, 1.5, 2, 5, 10]
-			result[epochs][string][temperature] = trainAndGenerate(None, pickle_model_path, string, None, temperature=temperature)
+			result[epochs][string][temperature] = trainAndGenerate(None, pickle_model_path, string, temperature=temperature)
 
 	pp.pprint(result[epochs])
 
