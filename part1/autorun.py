8258d2648b472db2784fabbf312f594a1e97f70e
rj-90 <rj.codesstuff@gmail.com>
2019-02-10 19:01:08 -0800

    trying to add useful pictures and remove the ones we aren't using

--- part1/autorun.py
+++ part1/autorun.py
@@ -7,27 +7,31 @@ pp = pprint.PrettyPrinter(indent=4)
 modelsFolder = "models/"
 pickle = ".pickle"
 
-inputFile="lordOfTheRingsReformatted.txt"
-pickle_model = "LOTR_model"
-epochs = 1
 
-string_starts = [ "Frodo ", "Sam", "When ", "It was", "As for ", "Mr. Bilbo Baggins ", "All that day "]
 
 results = {}
+for inputFileModelName in [("lordOfTheRingsReformatted.txt", "LOTR_model", [ "Frodo ", "Sam", "When ", "It was", "As for ", "Mr. Bilbo Baggins ", "All that day "]),
+	("Hamilton2.txt", "musical_model", [ "[HAMILTON]", "[ELIZA]", "[BURR]", "[COMPANY]", "[WASHINGTON]", "[LAFAYETTE]", "[HAMILTON/"]),
+	("GameOfThrones.txt", "GOT_model", [ "His father ", "Ned grimaced ", "Now it was ", "It was", "Ben Stark ", "She saw ", "Joffrey "])]:
 
-for epochs in [1, 3, 5, 10, 15, 25]:
-	test_name = "%s_e%s"%(pickle_model, epochs)
-	pickle_model_path = modelsFolder+test_name+pickle
-	trainAndGenerate(inputFile, None, None,
-	                epochs=epochs, checkpoint_dir=pickle_model_path )
+	inputFile=inputFileModelName[0]
+	pickle_model = inputFileModelName[1]
+	string_starts = inputFileModelName[2]
 
-	result[epochs] = {}
-	for string in string_starts:
-		result[epochs][string] = {}
-		for temperature in [0.01, 0.1, 0.5, 1.0, 1.5, 2, 5, 10]
-			result[epochs][string][temperature] = trainAndGenerate(None, pickle_model_path, string, temperature=temperature)
+	result[pickle_model] = {}
+	for epochs in [1, 3, 5, 10, 15, 25]:
+		test_name = "%s_e%s"%(pickle_model, epochs)
+		pickle_model_path = modelsFolder+test_name+pickle
+		trainAndGenerate(inputFile, None, None,
+		                epochs=epochs, checkpoint_dir=pickle_model_path )
 
-	pp.pprint(result[epochs])
+		result[pickle_model][epochs] = {}
+		for string in string_starts:
+			result[pickle_model][epochs][string] = {}
+			for temperature in [0.01, 0.1, 0.5, 1.0, 1.5, 2, 5, 10]
+				result[pickle_model][epochs][string][temperature] = trainAndGenerate(None, pickle_model_path, string, temperature=temperature, seq_length=10000)
+
+		pp.pprint(result[pickle_model][epochs])
 
 print(" Final Results ")
 pp.pprint(result)
